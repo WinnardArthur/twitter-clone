41bfd15e526863aed09c04aa3a6a4691c5c6e4b6
Winnard Arthur <winnardarthur7@gmail.com>
2023-07-01 18:26:47 +0000

    feat: create generic layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Header from "@/components/Header";
 import Head from "next/head";
 
 export default function Home() {
@@ -10,7 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h1 className="text-3xl text-white">Initial Setup</h1>
+        <Header label="Home" showBackArrow/>
       </main>
     </>
   );
