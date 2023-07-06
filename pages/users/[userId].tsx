59854a7bfab62350d28344b94df788fbf6c0bf85
Winnard Arthur <winnardarthur7@gmail.com>
2023-07-06 23:16:58 +0000

    feat: fetch posts of user on user details page

--- pages/users/[userId].tsx
+++ pages/users/[userId].tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/Header";
+import PostFeed from "@/components/posts/PostFeed";
 import UserBio from "@/components/users/UserBio";
 import UserHero from "@/components/users/UserHero";
 import useUser from "@/hooks/useUser";
@@ -22,8 +23,9 @@ const UserView = () => {
   return (
     <div>
       <Header label={fetchedUser?.name || "User Profile"} showBackArrow />
-          <UserHero userId={userId as string} />
-          <UserBio userId={userId as string} />
+      <UserHero userId={userId as string} />
+      <UserBio userId={userId as string} />
+      <PostFeed userId={userId as string} />
     </div>
   );
 };
