92937d10981cc29d669cbd7b9cd1ada1d2c40da7
Winnard Arthur <winnardarthur7@gmail.com>
2023-07-08 17:27:31 +0000

    feat: create comment post

--- components/Form.tsx
+++ components/Form.tsx
@@ -28,7 +28,8 @@ const Form: React.FC<FormProps> = ({ placeholder, isComment, postId }) => {
     try {
       setIsLoading(true);
 
-      await axios.post("/api/posts", { body });
+      const url = isComment ? `/api/comments?postId=${postId}` : '/api/posts'
+      await axios.post(url, { body });
 
       toast.success("Tweet created");
       setBody("");
@@ -39,7 +40,7 @@ const Form: React.FC<FormProps> = ({ placeholder, isComment, postId }) => {
     } finally {
       setIsLoading(false);
     }
-  }, [body, mutatePosts]);
+  }, [body, isComment, postId, mutatePosts]);
 
   return (
     <div className="border-b-[1px] border-neutral-800 px-5 py-2">
